7619521125e0fd9d746f3767a21c78e152aa9252
Jaren-Wheeler <git@github.com>
2025-03-21 15:33:08 -0600

    slight syntax change

--- src/view/DashboardView.java
+++ src/view/DashboardView.java
@@ -13,18 +13,18 @@ public class DashboardView {
     private JPanel dashboard;
 
     public DashboardView(JFrame window) {
-        this.window = window; 
+        this.window = window; // this is the same window as the login page
     }
 
     // create the main outline of the dashboard
     public void initializeDashboard() {
        
-        dashboard = new JPanel();
+        dashboard = new JPanel(); // new panel for dashboard
         dashboard.setLayout(new FlowLayout());
         JLabel db_title = new JLabel("Welcome");
         
         dashboard.add(db_title);
-        window.setContentPane(dashboard);
+        window.setContentPane(dashboard); // set the content of the window to dashboard panel rather than login panel
         window.setVisible(true);
     }
 }
